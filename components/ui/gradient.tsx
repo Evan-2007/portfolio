4e2c8e5ce52e4c73c42e83a20a003a1a27abbd55
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2024-03-04 02:01:20 -0800

    Add new UI components and styles

---
+++ components/ui/gradient.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "@/styles/gradient.module.css"; // Import CSS module
+
+const AnimatedBall = () => {
+  return <div className={`${styles.ball} w-72 h-72`}></div>;
+};
+
+export default AnimatedBall;
---
+++ components/ui/scroll-arrow.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { motion } from 'framer-motion';
+
+export default function ScrollArrow() {
+    return (
+        <div className="absolute flex items-center justify-center">
+        <div className="animate-bounce w-6 h-6">
+            <div className="absolute">
+                <ChevronDown size={160} strokeWidth={0.5}/>
+            </div>
+                <motion.div
+                initial={{ opacity: 0, y: 0 }}
+                animate={{ opacity: [.5, 0], y: [0, -30] }}
+                transition={{ duration: 1, repeat: Infinity, repeatDelay: 0 }}
+                className="absolute"
+                >
+                <ChevronDown size={160} strokeWidth={0.5}/>
+                </motion.div>
+        </div>
+
+      </div>
+    );
+}
+
+
+{/* <div className="animate-bounce w-6 h-6">
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+>
+    <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M19 14l-7 7m0 0l-7-7m7 7V3"
+    />
+</svg>
+</div> */}
