9e858bd3f86974ec03b28e089d4029449c697d29
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2024-03-05 19:44:30 -0800

    Update tsconfig.json, astro.config.mjs, Layout.astro, and package.json

---
+++ src/components/scrollarrow.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { motion } from "framer-motion";
+import styles from "../styles/react/animations.module.css";
+
+export default function ScrollArrow() {
+  return (
+    <div className="absolute flex items-center justify-center">
+      <div className="animate-bounce w-6 h-6 flex justify-center">
+        <div className="absolute">
+          <ChevronDown size={160} strokeWidth={0.5} color="white"/>
+        </div>
+        <div className={styles.fade}>
+          <ChevronDown size={160} strokeWidth={0.5} color="white" />  
+        </div>
+      </div>
+    </div>
+  );
+}
