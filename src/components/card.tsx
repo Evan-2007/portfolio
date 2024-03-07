4e3f984fd413a22e12eb6c7de8c9376c68c53a0d
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2024-03-06 22:27:51 -0800

    Refactor card component and add new content cards

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,60 +12,51 @@ import { Languages } from "lucide-react";
 
 export default function Card() {
   return (
-    <div className="flex flex-row flex-wrap mr-10 ml-10 mt-20 mb-10 justify-center items-start">
-      <CardContainer className="inter-var">
-        <CardItem>
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl pt-5 pb-5 dark:hover:shadow-emerald-500/[0.1]  dark:bg-black dark:border-white/[0.2] border-black/[0.1] flex justify-center flex-col align-middle text-center mr-3 ml-3 mb-6 items-center h-auto rounded-xl border  ">
-            <img src={JavaScript.src} alt="" className="h-20 w-20" />
-            <h1 className="lg:text-4xl lg:w-[240px] md:w-[140px] sm:w-[150px] text-xl w-[120px] mt-6 font-bold text-white">JavaScript</h1>
-          </CardBody>
-        </CardItem>
-      </CardContainer>
+    <div className="flex flex-row flex-wrap mr-10 ml-10 mt-10 mb-10 justify-center items-start md:mt-10 lg:mt-20">
+      <CardContent img={TypeScript.src} title="TypeScript" />
+      <CardContent img={JavaScript.src} title="JavaScript" />
+      <CardContent img={Next.src} title="Next.js" />
+      <CardContent img={Astro.src} title="Astro.js" />
+      <CardContent img={Nuxt.src} title="Nuxt.js" />
+      <CardContent img={Nest.src} title="Nest.js" />
 
-      <CardContainer className="inter-var">
-        <CardItem>
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl pt-5 pb-5 dark:hover:shadow-emerald-500/[0.1]  dark:bg-black dark:border-white/[0.2] border-black/[0.1] flex justify-center flex-col align-middle text-center mr-3 ml-3 mb-6 items-center h-auto rounded-xl border  ">
-            <img src={TypeScript.src} alt="" className="h-20 w-20" />
-            <h1 className="lg:text-4xl lg:w-[240px] md:w-[140px] sm:w-[150px] text-xl w-[120px] mt-6 font-bold text-white">TypeScript</h1>
-          </CardBody>
-        </CardItem>
-      </CardContainer>
-
-      <CardContainer className="inter-var">
-        <CardItem>
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl pt-5 pb-5 dark:hover:shadow-emerald-500/[0.1]  dark:bg-black dark:border-white/[0.2] border-black/[0.1] flex justify-center flex-col align-middle text-center mr-3 ml-3 mb-6 items-center h-auto rounded-xl border  ">
-            <img src={Next.src} alt="" className="h-20 w-20" />
-            <h1 className="lg:text-4xl lg:w-[240px] md:w-[140px] sm:w-[150px] text-xl w-[120px] mt-6 font-bold text-white">Next.js</h1>
-          </CardBody>
-        </CardItem>
-      </CardContainer>
-
-      <CardContainer className="inter-var">
-        <CardItem>
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl pt-5 pb-5 dark:hover:shadow-emerald-500/[0.1]  dark:bg-black dark:border-white/[0.2] border-black/[0.1] flex justify-center flex-col align-middle text-center mr-3 ml-3 mb-6 items-center h-auto rounded-xl border  ">
-            <img src={Astro.src} alt="" className="h-20 w-20" />
-            <h1 className="lg:text-4xl lg:w-[240px] md:w-[140px] sm:w-[150px] text-xl w-[120px] mt-6 font-bold text-white">Astro.js</h1>
-          </CardBody>
-        </CardItem>
-      </CardContainer>
-
-      <CardContainer className="inter-var">
-        <CardItem>
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl pt-5 pb-5 dark:hover:shadow-emerald-500/[0.1]  dark:bg-black dark:border-white/[0.2] border-black/[0.1] flex justify-center flex-col align-middle text-center mr-3 ml-3 mb-6 items-center h-auto rounded-xl border  ">
-            <img src={Nuxt.src} alt="" className="h-20 w-20" />
-            <h1 className="lg:text-4xl lg:w-[240px] md:w-[140px] sm:w-[150px] text-xl w-[120px] mt-6 font-bold text-white">Nuxt.js</h1>
-          </CardBody>
-        </CardItem>
-      </CardContainer>
-
-      <CardContainer className="inter-var">
-        <CardItem>
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl pt-5 pb-5 dark:hover:shadow-emerald-500/[0.1]  dark:bg-black dark:border-white/[0.2] border-black/[0.1] flex justify-center flex-col align-middle text-center mr-3 ml-3 mb-6 items-center h-auto rounded-xl border  ">
-            <img src={Nest.src} alt="" className="h-20 w-20" />
-            <h1 className="lg:text-4xl lg:w-[240px] md:w-[140px] sm:w-[150px] text-xl w-[120px] mt-6 font-bold text-white">Nest.js</h1>
-          </CardBody>
-        </CardItem>
-      </CardContainer>
     </div>
+  );
+}
+
+
+import linux from "../assets/linux.svg";
+import git from "../assets/git.svg";
+import tailwind from "../assets/tailwind.svg";
+import docker from "../assets/docker.svg";
+import cloudflare from "../assets/cloudflare.svg";
+import digitalOcean from "../assets/digitalOcean.svg";
+import vercel from "../assets/vercel.svg";
+import railway from "../assets/railway.svg";
+
+
+export function Other() {
+  return (
+
+    <div className="flex flex-row flex-wrap mr-10 ml-10 mt-10 mb-10 justify-center items-start md:mt-10 lg:mt-10">
+      <CardContent img={linux.src} title="Linux" />
+      <CardContent img={git.src} title="Git" />
+      <CardContent img={tailwind.src} title="Tailwind" />
+      <CardContent img={docker.src} title="Docker" />
+      <CardContent img={cloudflare.src} title="Cloudflare" />
+      <CardContent img={digitalOcean.src} title="DigitalOcean" />
+      <CardContent img={vercel.src} title="Vercel" />
+      <CardContent img={railway.src} title="Railway" />
+    </div>
+  );
+}
+
+
+function CardContent(props: { img: string; title: string }) {
+  return (
+    <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl pt-5 pb-5 dark:hover:shadow-emerald-500/[0.1]  dark:bg-black dark:border-white/[0.2] border-black/[0.1] flex justify-center flex-col align-middle text-center mr-3 ml-3 mb-6 items-center h-auto rounded-xl border  ">
+      <img src={props.img} alt="" className="h-20 w-20" />
+      <h1 className="lg:text-4xl lg:w-[240px] md:w-[140px] sm:w-[150px] text-xl w-[120px] mt-6 font-bold text-white">{props.title}</h1>
+    </CardBody>
   );
 }
