8a29926e2ce7e256eab144f61c700e2e406d0f7c
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2024-03-29 02:32:34 -0700

    Add ParticlesBackground component

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -3,14 +3,15 @@ import ScrollArrow from "./scrollarrow";
 import { Gradiant } from "./gradiant-ball";
 import React from "react";
 import styles from "../styles/react/gradient.module.css";
+import ParticlesBackground from "./ui/particals";
 
 export default function Title() {
   return (
     <div className="w-full h-[100vh]">
-      <div
-        className={`${styles.gradiant} w-full h-96 absolute ml-0 mr-0`}
-      ></div>
 
+      <div className="w-screen h-screen z-5 absolute">
+        <ParticlesBackground />
+        </div>
       <div className="w-full  absolute">
         <motion.div
           initial={{ opacity: 0 }}
