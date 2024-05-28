189f4c0b6167beb81485678f008aa97b382db350
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2024-05-28 13:40:40 -0700

    fixed typo in ocnfig so sitemap and robots.txt gets generated on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  side: 'https://evanc.dev',
+  site: 'https://evanc.dev',
   integrations: [tailwind({
     applyBaseStyles: true
   }), react(), robotsTxt({
