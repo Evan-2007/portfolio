63e9b3b92e61d10d7a1efa34ac1673eb7a6d0e10
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2025-12-10 02:07:50 -0800

    Add priority attribute to Image component in ProfilePicture for improved loading performance

--- src/components/profile-image.tsx
+++ src/components/profile-image.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function ProfilePicture() {
   return (
     <div className="w-[10vh] h-[10vh] rounded-full overflow-hidden max-md:mx-5 md:mx-0 md:w-[25vh] md:h-[25vh]">
-      <Image src={'https://avatars.githubusercontent.com/u/65788884?s=40&v=4'} alt="Profile Picture" width={128} height={128} className="w-full h-full object-cover" />
+      <Image priority src={'https://avatars.githubusercontent.com/u/65788884?s=40&v=4'} alt="Profile Picture" width={128} height={128} className="w-full h-full object-cover" />
     </div>
   );
 }
