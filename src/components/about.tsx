bf35bbaa284405e7280e115fd922a51aef600323
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2025-12-10 01:38:14 -0800

    Update About component text size for improved readability

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,10 +4,10 @@ export function About() {
       <h1 className="text-4xl font-bold text-white mb-4">
         Hello,
       </h1>
-      <p className="text-lg text-gray-300 max-w-xl mx-auto">
+      <p className="text-xl text-gray-300 max-w-xl mx-auto  tracking-wide">
         My name is Evan Cooper, a self-taught developer currently attending community college in Northern California.
       </p>
-      <p className="text-lg text-gray-300 max-w-xl mx-auto mt-4">
+      <p className="text-xl text-gray-300 max-w-xl mx-auto  tracking-wide mt-4">
         I am currently working on a few small personal projects and attempting to learn some electronics in my spare time.
       </p>
     </div>
